42fd149284b4ecb283fab4b3eb0dcae4f7c2eaf0
Vladimir Bakhanovich <139905364+vvbakhanovich@users.noreply.github.com>
2024-07-17 10:00:22 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/kardomoblieapp/KardoMoblieAppApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.kardomoblieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KardoMoblieAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KardoMoblieAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yandex/kardomoblieapp/KardoMoblieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.kardomoblieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KardoMoblieAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
