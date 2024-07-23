af16e5fad8c6d0983d0d7fb9b859e6c832c729fb
vvbakhanovich <vv.bakh@yandex.ru>
2024-07-23 10:22:50 +0300

    refactor: Добавить поля в UserDto

--- src/main/java/ru/yandex/kardomoblieapp/user/dto/UserDto.java
+++ src/main/java/ru/yandex/kardomoblieapp/user/dto/UserDto.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.yandex.kardomoblieapp.datafiles.model.DataFile;
+import ru.yandex.kardomoblieapp.user.model.Gender;
+
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -13,5 +17,25 @@ public class UserDto {
 
     private Long id;
 
+    private String name;
+
+    private String secondName;
+
+    private String surname;
+
+    private LocalDate dateOfBirth;
+
     private String email;
+
+    private String country;
+
+    private String city;
+
+    private Gender gender;
+
+    private DataFile profilePicture;
+
+    private String overview;
+
+    private String website;
 }
